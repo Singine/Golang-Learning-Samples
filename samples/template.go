2bea83426ee5a24e47b4571af6bea333c252ebb8
DZH <singine@live.cn>
2022-02-11 23:52:42 +0800

    updating sample template

--- samples/template.go
+++ samples/template.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+type UserInfo struct {
+	Name string
+	Gender string
+	Age int
+}
+
 func saybao(w http.ResponseWriter, r *http.Request)  {
 	//解析模板
 	t,err := template.ParseFiles("./template.tmpl")
@@ -15,8 +21,12 @@ func saybao(w http.ResponseWriter, r *http.Request)  {
 	}
 
 	//渲染模板
-	str := "Gubao"
-	err = t.Execute(w, str)
+	u1 :=UserInfo{
+		Name: "Gubao",
+		Gender: "Baby",
+		Age: 4,
+	}
+	err = t.Execute(w,  u1)
 	if err!= nil{
 		fmt.Println("Parse template failed, err:%v", err)
 		return
