9260ec9993b9be19f674cd2923aaf473dd681539
DZH <singine@live.cn>
2022-02-13 03:10:32 +0800

    updating go gin sample of url redirect

---
+++ samples/gin/redirect.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.GET("/index", func(context *gin.Context) {
+		//context.JSON(http.StatusOK, gin.H{
+		//	"status": "ok",
+		//})
+		context.Redirect(http.StatusMovedPermanently, "/login")
+	})
+
+	engine.GET("/redirect", func(context *gin.Context) {
+		context.Request.URL.Path = "/destination" //修改请求的地址
+		engine.HandleContext(context) //继续后续处理
+	})
+	engine.GET("/destination", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"status": "done!",
+		})
+	})
+
+	engine.Run(":5555")
+}
